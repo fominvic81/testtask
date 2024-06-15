007708224b2688ed96df45a40aa0e6f10a96ad16
fominvic81 <fomin.vic81@gmail.com>
2024-06-15 23:30:03 +0300

    Improve order of links in admin sidebar

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -5,16 +5,16 @@
         <div class="bg-gray-300">
             <a class="block w-full text-center py-2 bg-inherit hover:brightness-105 transition-all" href="{{ route('home') }}">Головна</a>
             <a class="block w-full text-center py-2 bg-inherit hover:brightness-105 transition-all" href="{{ route('editors.edit', Auth::user()) }}">Редагувати профіль</a>
-            <a class="block w-full text-center py-2 bg-inherit hover:brightness-105 transition-all" href="{{ route('articles.my') }}">Мої новини</a>
             <a class="block w-full text-center py-2 bg-inherit hover:brightness-105 transition-all" href="{{ route('articles.create')}}">Створити новину</a>
-            @can('view-any', \App\Models\Editor\Editor::class)
-            <a class="block w-full text-center py-2 bg-inherit hover:brightness-105 transition-all" href="{{ route('editors.index') }}">Редактори</a>
-            @endcan
             @can('create', \App\Models\Editor\Editor::class)
             <a class="block w-full text-center py-2 bg-inherit hover:brightness-105 transition-all" href="{{ route('editors.create') }}">Створити Редактора</a>
             @endcan
+            <a class="block w-full text-center py-2 bg-inherit hover:brightness-105 transition-all" href="{{ route('articles.my') }}">Мої новини</a>
             @can('view-any', \App\Models\Article\Article::class)
             <a class="block w-full text-center py-2 bg-inherit hover:brightness-105 transition-all" href="{{ route('articles.index') }}">Всі новини</a>
+            @endcan
+            @can('view-any', \App\Models\Editor\Editor::class)
+            <a class="block w-full text-center py-2 bg-inherit hover:brightness-105 transition-all" href="{{ route('editors.index') }}">Редактори</a>
             @endcan
             <form class="block bg-inherit" action="{{ route('logout' )}}" method="POST">
                 @csrf
